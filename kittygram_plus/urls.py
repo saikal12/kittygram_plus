92cdd6f7080f357540d27fb537d42833d9c1ae08
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2023-07-07 17:34:33 +0300

    settings

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
@@ -12,4 +12,8 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path(
+        'api-token-auth/',
+        views.obtain_auth_token
+    ),
 ]
